6bfdd8dd6e3b51cfc68e367dc1b6c10265678333
sakib124024 <sakib124024@yahoo.com>
2024-04-25 15:41:57 +0100

    pattern matching 2

--- src/main/java/com/oocode/Answerer.java
+++ src/main/java/com/oocode/Answerer.java
@@ -4,7 +4,17 @@ import java.util.Arrays;
 public class Answerer {
     public String answerFor(String question) {
         if (question.startsWith("Which of the following numbers is the largest: ")){
-            return "97";
+            String[] numbers = question.replaceAll("[^0-9,]","").split(",");
+            int maxNumber = Integer.MIN_VALUE;
+
+            for (String number : numbers) {
+                int currentNumber = Integer.parseInt(number.trim());
+                if (currentNumber > maxNumber) {
+                    maxNumber = currentNumber;
+                }
+            }
+
+            return String.valueOf(maxNumber);
         }
         if (question.startsWith("What is your name?")){
             return "AN TEAM";
